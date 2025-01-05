5b78322f6e32faeb1db1648f2869068ad39d62fb
DrK-ToN <everton.drk@gmail.com>
2025-01-04 21:58:53 -0300

    Criação do controle do modelo de usuarios

---
+++ controllers/userControll.js
@@ -0,0 +1,10 @@
+const userModel = require("../models/User");
+
+function renderUsers(req, res) {
+    const users = userModel.getUsers();
+    res.render("lista", { users });
+}
+
+modelu.exports = {
+    renderUsers,
+};
